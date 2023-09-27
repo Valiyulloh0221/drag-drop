c6f901687e0fbf82a183632e2593d767a8765b66
Valiyulloh0221 <lvov11190@gmail.com>
2023-09-27 12:54:04 +0500

    Drag & drop

---
+++ src/main.js
@@ -0,0 +1,104 @@
+let btnTodo     = document.getElementById("todo-btn");
+let btnProgress = document.getElementById("progress-btn");
+let btnDone     = document.getElementById("done-btn")
+
+const progsBoard= document.querySelector(".progress-board")
+const todoBoard = document.querySelector(".todo-board")
+const doneBoard = document.querySelector(".done-board")
+const cards     = document.querySelectorAll(".card1");
+
+const left   = document.getElementById("left");
+const center = document.getElementById("center");
+const right  = document.getElementById("right");
+
+
+
+function allowDrop(e) {
+    e.preventDefault();
+  }
+
+  function drag(e) {
+    e.dataTransfer.setData("number", e.target.id);
+  }
+
+  function drop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("number");
+    e.target.appendChild(document.getElementById(data));
+  }
+
+let progNum = 5;
+let todoNum = 5;
+let doneNum = 5;
+let idNum   = 1;
+
+btnProgress.addEventListener("click" , ()=>{
+    const cardProgress = document.createElement("div")
+    cardProgress.classList = "card"
+    cardProgress.id = `drag${idNum}`
+    cardProgress.setAttribute("draggable" , "true")
+    cardProgress.setAttribute("ondragstart" , "drag(event)")
+    progsBoard.  appendChild(cardProgress);
+    cardProgress.innerText = `Progress #${progNum}`
+    progNum += 1
+    idNum   += 1
+})
+
+
+btnTodo.addEventListener("click" , ()=>{
+    const cardTodo = document.createElement("div")
+    cardTodo.classList = "card"
+    cardTodo.id = `drag${idNum}`
+    cardTodo. setAttribute("draggable" , "true")
+    cardTodo. setAttribute("ondragstart" , "drag(event)" )
+    todoBoard.appendChild(cardTodo);
+    cardTodo.innerText = `Todo #${todoNum}`;
+    todoNum += 1
+    idNum   += 1
+})
+
+
+btnDone.addEventListener("click" , ()=>{
+    const cardDone = document.createElement("div")
+    cardDone.classList = "card"
+    cardDone.id = `drag${idNum}`
+    cardDone.setAttribute("draggable" , "true")
+    cardDone.setAttribute("ondragstart" , "drag(event)")
+    doneBoard.appendChild(cardDone);
+    cardDone.innerText = `done #${doneNum}`;
+    doneNum += 1;
+    idNum   += 1;
+})
+
+
+
+
+for (let card1 of cards) {
+  card1.addEventListener("dragstart", function (e) {
+    let selected = e.target;
+
+    right.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    right.addEventListener("drop", function (e) {
+      right.appendChild(selected);
+      selected = null;
+    });
+
+    center.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    center.addEventListener("drop", function (e) {
+      center.appendChild(selected);
+      selected = null;
+    });
+
+    left.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    left.addEventListener("drop", function (e) {
+      left.appendChild(selected);
+      selected = null;
+    });
+  });
+}
